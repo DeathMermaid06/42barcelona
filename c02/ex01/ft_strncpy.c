a37793565709ba13ebd7d1d84856585a005ea38c
Mariela Pistasoli <106203272+DeathMermaid06@users.noreply.github.com>
2023-12-07 14:49:14 +0100

    c02 correcciones

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -1,17 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpistaso <mpistaso@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 15:31:09 by mpistaso          #+#    #+#             */
+/*   Updated: 2023/12/06 15:42:33 by mpistaso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int	i;
 
-    i = 0;
-    while (i < n && src[i] != '\0' && src[i] != ' ')
-    {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
-return (dest);
+	i = 0;
+	while (i < n && src[i] != '\0' && dest[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,36 +1,39 @@
-char *ft_strcapitalize(char *str)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpistaso <mpistaso@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 15:34:47 by mpistaso          #+#    #+#             */
+/*   Updated: 2023/12/06 16:53:00 by mpistaso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcapitalize(char *str)
 {
-    int i = 0;
+	int	i;
 
-    while (str[i] != '\0')
-    {
-        if (str[i] >= 65 && str[i] <= 90)
-        {       
-                str[i] = str[i] + 32;       
-        }
-	i++;
-    }
-
-    if (str[0] >= 97 && str[0] <= 122)
-    {
-        str[0] = str[0] - 32;
-        i++;
-    }
-
-	i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' ' || str[i] == '-' || str[i] == '+')
-        {
-            if (str[i + 1] >= 97 && str[i + 1] <= 122)
-            {
-                str[i + 1] = str[i + 1] - 32;
-            }
-        }
-        i++;
-    }
-
-    return (str);
+	i = 1;
+	if (str[0] >= 97 && str[0] <= 122)
+	{
+		str[0] = str[0] - 32;
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ' || str[i] == '-' || str[i] == '+' || str[i] == '.')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+				i++;
+			}
+		}
+		else if (str[i] >= 65 && str[i] <= 90)
+		{
+			str[i] = str[i] + 32;
+		}
+		i++;
+	}
+	return (str);
 }
-
-
